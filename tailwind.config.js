c51271f2156c6e9ad42a686fb11d4cdbfd356a59
Michael Chen <yinchen618@gmail.com>
2024-02-04 23:36:51 +0800

    update

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+module.exports = {
+  mode: 'jit',
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+
+  },
+  variants: {},
+  plugins: [],
+}
