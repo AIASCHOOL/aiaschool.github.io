84e21edc97b91f49549159bc79b660a0093fdc51
Michael Chen <yinchen618@gmail.com>
2024-04-12 02:24:23 +0800

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,14 @@ module.exports = {
   mode: 'jit',
   content: ["./src/**/*.{html,js}"],
   theme: {
-
+    extend: {
+      fontSize: {
+        'base': '1.75rem'
+        // '2xl': '1.75rem',
+        // '2xl': '1.75rem',
+        // '3xl': '2rem',
+      },
+    },
   },
   variants: {},
   plugins: [],
