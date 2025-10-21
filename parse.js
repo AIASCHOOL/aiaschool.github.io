13439261ff904696ed3ec5a577f006a9df2731eb
Yin Chen Chen <yinchen618@gmail.com>
2025-10-21 12:16:12 +0800

    upfate

--- parse.js
+++ parse.js
@@ -1,17 +1,29 @@
 Parse.initialize('myappID', 'infowin');
 Parse.serverURL = 'https://parse3.infowin.com.tw/parse';
 
-async function read() {
+async function read(page = 1, limit = 10) {
   var News = Parse.Object.extend("aiasoNews");
   var query = new Parse.Query(News);
-  // query.equalTo("name", textName);
   query.descending("publishDate");
-  // query.first().then(function (pet) {
+  
+  // 计算跳过的记录数
+  var skip = (page - 1) * limit;
+  query.skip(skip);
+  query.limit(limit);
+  
   var snap = await query.find().catch(function (error) {
     console.log("Error: " + error.code + " " + error.message);
   })
   const obj = snap.map(data => data.toJSON());
-  // console.table(obj)
   return obj;
+}
 
+async function getTotalCount() {
+  var News = Parse.Object.extend("aiasoNews");
+  var query = new Parse.Query(News);
+  var count = await query.count().catch(function (error) {
+    console.log("Error: " + error.code + " " + error.message);
+    return 0;
+  });
+  return count;
 }
